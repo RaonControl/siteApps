135d6aba70f350b8377d4038cad60751c85ed27b
scwook <pi@raspberrypi>
2015-01-08 17:57:09 +0900

    Update Sequence Code

--- raspSTEP/CSS/user_move2.py
+++ raspSTEP/CSS/user_move2.py
@@ -4,5 +4,5 @@ name_user_pos="text_user_pos2"
 
 val_user_pos=display.getWidget(name_user_pos).getPropertyValue("text")
 
-PVUtil.writePV("STEP:ABS", val_user_pos)
+PVUtil.writePV("STEP:ANG:ABS", val_user_pos)
 PVUtil.writePV("STEP:MOVA", "1")
