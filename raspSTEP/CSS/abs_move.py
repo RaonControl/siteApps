0cfb167566fa1bc6795ed09f13d9fc0363650200
scwook <scwook@scwook.risp.net>
2015-01-12 09:52:51 +0900

    Fixed moving and direction error

--- raspSTEP/CSS/abs_move.py
+++ raspSTEP/CSS/abs_move.py
@@ -1,8 +1,11 @@
 from org.csstudio.opibuilder.scriptUtil import PVUtil
+import time
 
 name_abs_pos="text_abs_pos"
 
 val_abs_pos=display.getWidget(name_abs_pos).getPropertyValue("text")
 
 PVUtil.writePV("STEP:ANG:ABS", val_abs_pos)
+time.sleep(0.5);
+
 PVUtil.writePV("STEP:MOVA", "1")
