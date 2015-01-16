b6e28ebaf99f7518d35fb436c348a7bef9db55c5
mijoy <mijoy0909@gmail.com>
2015-01-16 17:46:47 +0900

    Friday, January 16 17:46:50 KST 2015

--- snmp2/iocBoot/iocsnmp2/st.cmd
+++ snmp2/iocBoot/iocsnmp2/st.cmd
@@ -13,9 +13,7 @@ snmp2_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=mijoy0909Host")
-#dbLoadRecords("db/snmp2.vdb","USER=mijoy0909Host")
-#dbLoadRecords("db/snmp.vdb","USER=mijoy0909Host")
-dbLoadRecords("db/snmp_set.vdb", "USER=mijoy0909Host")
+dbLoadRecords("db/snmp.vdb", "USER=mijoy0909Host")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
