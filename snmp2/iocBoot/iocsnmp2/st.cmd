2158dd57be62c1b8593775405a2073f2377e9bf4
mijoy <mijoy0909@gmail.com>
2015-01-16 17:00:22 +0900

    Friday, January 16 17:00:24 KST 2015
    modified snmp2

--- snmp2/iocBoot/iocsnmp2/st.cmd
+++ snmp2/iocBoot/iocsnmp2/st.cmd
@@ -14,10 +14,8 @@ snmp2_registerRecordDeviceDriver pdbbase
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=mijoy0909Host")
 #dbLoadRecords("db/snmp2.vdb","USER=mijoy0909Host")
-
-dbLoadRecords("db/snmp.vdb","USER=mijoy0909Host")
-#dbLoadRecords("db/snmp2.vdb","USER=mijoy0909Host, ip=10.1.5.142, name=mijoy, oids=PowerNet-MIB::sPDUOutletCtl.8")
-#dbLoadRecords("db/snmp2_1.vdb","USER=mijoy0909Host, ip=10.1.5.142, name=mijoy, oids=PowerNet-MIB::sPDUOutletPowerOnTime.8")
+#dbLoadRecords("db/snmp.vdb","USER=mijoy0909Host")
+dbLoadRecords("db/snmp_set.vdb", "USER=mijoy0909Host")
 
 cd ${TOP}/iocBoot/${IOC}
 iocInit
