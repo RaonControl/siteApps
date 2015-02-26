33ef2880af9686441f29443d4acc8856fcaeca95
mijoy <mijoy0909@gmail.com>
2015-02-26 17:30:41 +0900

    Thursday, February 26 17:30:44 KST 2015
    modified path of mibs

--- snmp2/iocBoot/iocsnmp2/st.cmd
+++ snmp2/iocBoot/iocsnmp2/st.cmd
@@ -7,7 +7,7 @@
 
 cd ${TOP}
 
-epicsEnvSet("MIBDIRS", "+$(TOP)/mibs")
+epicsEnvSet("MIBDIRS", "+${EPICS_PATH}/siteLibs/snmpLib2/mibs")
 
 ## Register all support components
 dbLoadDatabase "dbd/snmp2.dbd"
