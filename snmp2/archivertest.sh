c26f49cf9d5e08ea37394003efea0f91dd9c8dad
mijoy <mijoy0909@gmail.com>
2015-03-24 16:40:15 +0900

    Tuesday, March 24 16:40:16 KST 2015
    fixed test script

--- snmp2/archivertest.sh
+++ snmp2/archivertest.sh
@@ -3,39 +3,58 @@
 while [ : ]; do  
 #++
 for i in {1200..3000..100};do
+stat=`caget APC:PDU2_Outlet8_R`
 echo "***************************************************************"
 caget SNMPIOC:TIMESTAMP
+
 echo "---------------------------------------------------------------"
-echo "caget ---------------------------------------------------------"
-caget WIENER:CRATE3_MainPower_R WIENER:CRATE3_PS_Status WIENER:CRATE3_FanairTemp WIENER:CRATE3_FanNominalSpeed_R WIENER:CRATE3_FanNominalSpeed_W APC:PDU2_Outlet8_R APC:PDU2_Outlet8_W 
 echo "snmpget -------------------------------------------------------"
 snmpget -v 1 -c public 10.1.5.123 WIENER-CRATE-MIB::sysMainSwitch.0  WIENER-CRATE-MIB::sysStatus.0  WIENER-CRATE-MIB::fanAirTemperature.0 WIENER-CRATE-MIB::fanNominalSpeed.0
 snmpget -v 1 -c public 10.1.5.142 PowerNet-MIB::sPDUOutletCtl.8
+echo "---------------------------------------------------------------"
+echo "caget ---------------------------------------------------------"
+caget WIENER:CRATE3_MainPower_R WIENER:CRATE3_PS_Status WIENER:CRATE3_FanairTemp WIENER:CRATE3_FanNominalSpeed_R WIENER:CRATE3_FanNominalSpeed_W APC:PDU2_Outlet8_R APC:PDU2_Outlet8_W 
 echo "caput ---------------------------------------------------------"
+
+case "$stat" in
+"APC:PDU2_Outlet8_R             1")
+caput APC:PDU2_Outlet8_W 2;;
+"APC:PDU2_Outlet8_R             2")
+caput APC:PDU2_Outlet8_W 1;;
+*)
+echo "error";;
+esac
+
 caput WIENER:CRATE3_FanNominalSpeed_W $i
-caput APC:PDU2_Outlet8_W 1
-# echo "snmpset -------------------------------------------------------"
-# snmpset -v 3 -u admin -l authPriv -a MD5 -A MySecret -x DES -X MySecret 10.1.5.123 WIENER-CRATE-MIB::fanNominalSpeed.0 i $i
-# echo $i
-sleep 60
+sleep 10
 done
+
 
 #--
 for i in {2900..1300..100};do
 echo "***************************************************************"
 caget SNMPIOC:TIMESTAMP
 echo "---------------------------------------------------------------"
-echo "caget ---------------------------------------------------------"
-caget WIENER:CRATE3_MainPower_R WIENER:CRATE3_PS_Status WIENER:CRATE3_FanairTemp WIENER:CRATE3_FanNominalSpeed_R WIENER:CRATE3_FanNominalSpeed_W APC:PDU2_Outlet8_R APC:PDU2_Outlet8_W 
 echo "snmpget -------------------------------------------------------"
 snmpget -v 1 -c public 10.1.5.123 WIENER-CRATE-MIB::sysMainSwitch.0  WIENER-CRATE-MIB::sysStatus.0  WIENER-CRATE-MIB::fanAirTemperature.0 WIENER-CRATE-MIB::fanNominalSpeed.0
 snmpget -v 1 -c public 10.1.5.142 PowerNet-MIB::sPDUOutletCtl.8
+echo "---------------------------------------------------------------"
+echo "caget ---------------------------------------------------------"
+caget WIENER:CRATE3_MainPower_R WIENER:CRATE3_PS_Status WIENER:CRATE3_FanairTemp WIENER:CRATE3_FanNominalSpeed_R WIENER:CRATE3_FanNominalSpeed_W APC:PDU2_Outlet8_R APC:PDU2_Outlet8_W 
 echo "caput ---------------------------------------------------------"
+
+case "$stat" in
+"APC:PDU2_Outlet8_R             1")
+caput APC:PDU2_Outlet8_W 2;;
+"APC:PDU2_Outlet8_R             2")
+caput APC:PDU2_Outlet8_W 1;;
+*)
+echo "error";;
+esac
+
 caput WIENER:CRATE3_FanNominalSpeed_W $i
-caput APC:PDU2_Outlet8_W 2
-# echo "snmpset -------------------------------------------------------"
-# snmpset -v 3 -u admin -l authPriv -a MD5 -A MySecret -x DES -X MySecret 10.1.5.123 WIENER-CRATE-MIB::fanNominalSpeed.0 i $i
-# echo $i
-sleep 60
+sleep 10
 done
 done
+done
+
--- snmp2/pdu_test.sh
+++ snmp2/pdu_test.sh
@@ -4,7 +4,7 @@ while [ : ]; do
 
 #on
 echo "***************************************************************"
-echo `date`  
+caget SNMPIOC:TIMESTAMP
 echo "---------------------------------------------------------------"
 echo "caget ---------------------------------------------------------"
 caget APC:PDU2_Outlet8_R
@@ -13,20 +13,20 @@ echo "snmpget -------------------------------------------------------"
 snmpget -v 1 -c public 10.1.5.142 PowerNet-MIB::sPDUOutletCtl.8
 echo "caput ---------------------------------------------------------"
 caput APC:PDU2_Outlet8_W 1
-sleep 10
+sleep 60
 
 
 #off
 echo "***************************************************************"
-echo `date`  
+caget SNMPIOC:TIMESTAMP
 echo "---------------------------------------------------------------"
 echo "caget ---------------------------------------------------------"
 caget APC:PDU2_Outlet8_R
 caget APC:PDU2_Outlet8_W
 echo "snmpget -------------------------------------------------------"
-snmpget -v 1 -c public 10.1.5.142 PowerNet-MIB::sPDUOutletCtl.8
+snmpget -v 1 -c public 10.1.5.142 sPDUOutletCtl.8
 echo "caput ---------------------------------------------------------"
 caput APC:PDU2_Outlet8_W 2
-sleep 10
+sleep 60
 done
 
