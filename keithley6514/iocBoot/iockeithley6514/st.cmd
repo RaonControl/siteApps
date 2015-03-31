1f18ce6729a1b60628d9146749fe38284228f682
ctrluser <ctrluser@ecrispc0>
2015-03-31 15:41:30 +0900

    update for real configuration for keithley6514

--- keithley6514/iocBoot/iockeithley6514/st.cmd
+++ keithley6514/iocBoot/iockeithley6514/st.cmd
@@ -9,45 +9,17 @@ epicsEnvSet "STREAM_PROTOCOL_PATH" "$(TOP)/db"
 epicsEnvSet "SYS" "$(SYS=ECR11)"
 epicsEnvSet "SUBSYS" "$(SUBSYS=Diag)"
 epicsEnvSet "DEV" "$(DEV=FC01)"
-#epicsEnvSet "TTY" "$(TTY=/dev/ttyUSB0)"
-epicsEnvSet "TTY" "$(TTY=/dev/ttyS0)"
-epicsEnvSet "EPICS_CA_SERVER_PORT" "15065"
 
+#epicsEnvSet "EPICS_CA_SERVER_PORT" "15065"
 
 cd ${TOP}
 ## Register all support components
 dbLoadDatabase "dbd/keithley6514.dbd"
 keithley6514_registerRecordDeviceDriver pdbbase
 
-# drvAsynSerialPortConfigure("portName","ttyName",priority,noAutoConnect,noEos)
-# asynSetOption("portName",addr,"key","value")
-
-# priority      - Priority at which the asyn I/O thread will run. If this is zero or missing then epicsThreadPriorityMedium is used.
-# noAutoConnect - Zero or missing indicates that portThread should automatically connect. Non-zero if explicit connect command must be issued.
-# noEos         - Zero or missing indicates that the input routines should search for the end-of-string character(s), if any. Non-zero inhibits end-of-string search.
-# addr          - This argument is ignored since serial devices are configured with multiDevice=0.
-
-drvAsynSerialPortConfigure("L0","$(TTY)",0,0,0)
-asynSetOption("L0", -1, "baud", "57600") 
-asynSetOption("L0", -1, "bits", "8") 
-asynSetOption("L0", -1, "parity", "none") 
-asynSetOption("L0", -1, "stop", "1") 
-#
-# Ignore DTR / DCD : Y
-#
-asynSetOption("L0", -1, "clocal", "Y") 
-#
-# Ignore CTS / RTS : N - 6514 doesn't support HW handshaking
-#
-asynSetOption("L0", -1, "crtscts", "N") 
-#asynOctetSetInputEos("L0", -1, "\n") 
-#asynOctetSetOutputEos("L0", -1, "\n") 
-#asynSetTraceIOMask("L0",-1,0x2) 
-#asynSetTraceMask("L0",-1,0x9) 
-
+drvAsynIPPortConfigure("L0", "192.168.1.140:4013", 0, 0, 0)
 
 ## Load record instances
-#dbLoadRecords("db/xxx.db","user=jhleeHost")
 dbLoadRecords("db/devkeithley6514.db","SYS=$(SYS),SUBSYS=$(SUBSYS),DEV=$(DEV), PORT=L0,A=0") 
 dbLoadRecords("db/dbTimeStamp.db", "SYS=$(SYS),SUBSYS=$(SUBSYS),DEV=$(DEV)")
 
