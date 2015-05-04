12d79fb4168a3b33e99739d457eeb599b8dddf7b
scwook <scwook@scwook.risp.net>
2015-05-04 16:13:37 +0900

    Rearrange ecris css files

--- ecrisApps/css/user_position3.py
+++
@@ -1,8 +0,0 @@
-from org.csstudio.opibuilder.scriptUtil import PVUtil
-
-name_current_pulse="label_cur_pulse"
-name_user_pos="text_user_pos3"
-
-val_current_pulse=display.getWidget(name_current_pulse).getPropertyValue("text")
-
-display.getWidget(name_user_pos).setPropertyValue("text", val_current_pulse)
