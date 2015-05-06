bafaefc3cc9d8354ac9bb376174dde66c7a7b74c
Nam Seung Hee <namsh@ibs.re.kr>
2015-05-06 14:43:37 +0900

    integrated devIocState in snmpTest IOC

--- snmpTest/iocBoot/iocsnmpTest/st.cmd
+++ snmpTest/iocBoot/iocsnmpTest/st.cmd
@@ -13,9 +13,25 @@ snmpTest_registerRecordDeviceDriver pdbbase
 
 ## Load record instances
 #dbLoadRecords("db/xxx.db","user=mijoy0909Host")
-dbLoadRecords("db/xerox.vdb", "HOST=10.1.4.182, COM=public, V2C=SNMP_VERSION_2c, PR=Printer-MIB::")
+dbLoadRecords("db/xerox.vdb", "HOST=10.1.6.232, COM=public, V2C=SNMP_VERSION_2c, PR=Printer-MIB::")
 dbLoadRecords("db/timestamp.vdb", "S=SNMPIOC")
-dbLoadRecords "db/alive.db", "P=aliveEx:,RHOST=10.1.4.191"
+dbLoadRecords "db/alive.db", "P=aliveEx:,RHOST=10.1.6.104"
+
+
+#dbLoadDatabase("dbd/testIocStats.dbd",0,0)
+
+## Load all record instances (VxWorks)
+#dbLoadRecords("db/iocAdminVxWorks.db","IOC=IOCTEST")
+## or load only those records for RTEMS IOCs
+#dbLoadRecords("db/iocAdminRTEMS.db","IOC=IOCTEST")
+## or load only those records for Soft IOCs
+dbLoadRecords("db/iocAdminSoft.db","IOC=IOCTEST")
+## optionally load the SCAN monitoring records
+#dbLoadRecords("db/iocAdminScanMon.db","IOC=IOCTEST")
+#dbLoadRecords("db/testIocAdminRelease.db","IOC=IOCTEST")
+
+
+
 
 cd "${TOP}/iocBoot/${IOC}"
 iocInit
