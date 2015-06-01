923a3761ddf2cf03db76ac991288641dac40ccc4
Sangil-Lee <silee7103@gmail.com>
2015-06-01 18:42:45 +0900

    Implementing for interface between EPICS and MySQL

---
+++ rdbMySQL/rdbmysqlApp/src/rdbmySQLConnManager.h
@@ -0,0 +1,82 @@
+#ifndef __RDB_MYSQL_CONN_MANAGER_H
+#define __RDB_MYSQL_CONN_MANAGER_H
+
+#include <vector>
+#include <stdlib.h>
+#include <my_global.h>
+#include <mysql.h>
+#include "alarm.h"
+#include "cvtTable.h"
+#include "dbDefs.h"
+#include "dbAccess.h"
+#include "recGbl.h"
+#include "recSup.h"
+#include "devSup.h"
+#include "link.h"
+#include "menuScan.h"
+#include "rdbmySQLRecord.h"
+#include "epicsExport.h"
+#include "shareLib.h"
+#include "dbCommon.h"
+#include "dbScan.h"
+#include "callback.h"
+#include "cantProceed.h"
+#include "epicsStdio.h"
+#include "epicsMutex.h"
+#include "epicsEvent.h"
+#include "epicsThread.h"
+#include "iocsh.h"
+
+using namespace std;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+long read_init_record(void *);
+long write_init_record(void *);
+long read_rdb(void*);
+long write_rdb(void*);
+long getIoIntInfo(int cmd, dbCommon *pr, IOSCANPVT *iopvt);
+static void devRDBScanThread(void);
+
+
+epicsShareFunc int drvMySQLConnInit(const char *dbname, const char *user, const char *password, 
+		const char *hostaddr, const char *port );
+
+static void devMySQLScanThread(void);
+
+typedef struct devRdbmySQLSoft{
+	long		number;
+	DEVSUPFUN	report;
+	DEVSUPFUN	init;
+	DEVSUPFUN	init_record;
+	DEVSUPFUN	get_ioint_info;
+	//long  (*get_ioint_info) (int, dbCommon*, IOSCANPVT*);
+	DEVSUPFUN	read_rdb;
+}devRdbmySQLSoft;
+
+class MySQLConnManager {
+
+public:
+	MySQLConnManager();
+	~MySQLConnManager();
+
+	int Version();
+
+	//member function
+private:
+	int initialize();
+
+//member data
+private:
+	MYSQL *conn;
+		
+};
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __RDB_MYSQL_CONN_MANAGER_H
+
