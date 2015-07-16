835ec26e1e616123d490047b70752ae17d53e9e6
mijoy <mijoy0909@gmail.com>
2015-07-16 10:31:45 +0900

    Thursday, July 16 10:31:49 KST 2015 update css

--- snmp2/css/integrated_monitoring/xerox_state.js
+++ snmp2/css/integrated_monitoring/xerox_state.js
@@ -10,21 +10,17 @@ var state = pv2.trim();
 if (print == "onRequest(2)") {
 widget.setPropertyValue("on_label","Check printer");
 widget.setPropertyValue("on_color","Minor");
-widget.setPropertyValue("font",ColorFontUtil.getFont("Droid Sans",13,1));
 
 } else if (print == "unavailable(3)") {
 widget.setPropertyValue("on_label","Printer unavailable");
-widget.setPropertyValue("font",ColorFontUtil.getFont("Droid Sans",13,1));
 widget.setPropertyValue("on_color","Minor");
 
 } else if (print == "initializing(6)") {
 widget.setPropertyValue("on_label","Printer initializing");
-widget.setPropertyValue("font",ColorFontUtil.getFont("Droid Sans",13,1));
 widget.setPropertyValue("on_color","Invalid");
 
 } else if (print == "terminating(7)") {
 widget.setPropertyValue("on_label","Printer terminating");
-widget.setPropertyValue("font",ColorFontUtil.getFont("Droid Sans",13,1));
 widget.setPropertyValue("on_color","Invalid");
 
 } else if(print == "ready(1)") {
@@ -33,12 +29,10 @@ widget.setPropertyValue("on_color",ColorFontUtil.getColorFromRGB(0,255,0));
 
 } else if (state == "Copying...") {
 widget.setPropertyValue("on_label","Copying...");
-widget.setPropertyValue("font",ColorFontUtil.getFont("Droid Sans",13,1));
 widget.setPropertyValue("on_color",ColorFontUtil.getColorFromRGB(30,144,255));
 
 } else if (state == "Copy-User Action") {
 widget.setPropertyValue("on_label","Copying...");
-widget.setPropertyValue("font",ColorFontUtil.getFont("Droid Sans",13,1));
 widget.setPropertyValue("on_color",ColorFontUtil.getColorFromRGB(30,144,255));
 
 } else if (print == "busy(5)") {
@@ -51,6 +45,5 @@ widget.setPropertyValue("on_color",ColorFontUtil.getColorFromRGB(30,144,255));
 
 } else {
 widget.setPropertyValue("on_label","Check printer");
-widget.setPropertyValue("font",ColorFontUtil.getFont("Droid Sans",13,1));
 widget.setPropertyValue("on_color",ColorFontUtil.getColorFromRGB(255,0,0));
 }
